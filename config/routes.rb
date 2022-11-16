e7ca43acfaa0eaa16f6cfb4b254305fe0d802858
Ubuntu <ubuntu@ip-172-31-56-122.ec2.internal>
2022-11-16 09:05:32 +0000

    detailde_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-    #get 'top/main'
-    #root 'top#main'
+    get 'top/main'
+    root 'top#main'
     get 'moneys/expenditure_list'
+    get 'moneys/detailde_view'
 end
