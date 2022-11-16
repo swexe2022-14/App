9eab1bab0988a4906434c0eba54edfe0c1430463
2011140088n <2011140088n@ed.fuk.kindai.ac.jp>
2022-11-16 07:18:47 +0000

    login

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,3 +1,26 @@
 class TopController < ApplicationController
-    
+    def main
+        if session[:login_uid]
+            render "main"
+        else 
+            render "login"
+        end
+    end
+    def login
+        if User.find_by(uid: params[:uid])
+            signup = User.find_by(uid: params[:uid]).pass
+            if BCrypt::Password.new(signup) == params[:pass]
+                session[:login_uid] = params[:uid]
+                redirect_to root_path
+            else
+                render 'error'
+            end
+        else
+            render 'error'
+        end
+    end
+    def logout
+        session.delete(:login_uid)
+        redirect_to root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
     get 'top/main'
     root 'top#main'
+    post 'top/login'
+    get 'top/logout'
 end
