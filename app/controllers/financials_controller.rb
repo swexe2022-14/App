8f4071919229deb78c3da95d15d76e6cc0e8c378
2011140053 <ubuntu@ip-172-31-56-122.ec2.internal>
2022-12-14 08:16:20 +0000

    new_finance

--- app/controllers/financials_controller.rb
+++ app/controllers/financials_controller.rb
@@ -14,7 +14,29 @@ class FinancialsController < ApplicationController
         end
     end
     
+    def destroy
+        financial = Financial.find(params[:id])
+        financial.destroy
+        redirect_to financials_show_path
+    end
+    
     def show
         @financials = Financial.all
     end
+    
+    def edit
+        @financial = Financial.find(params[:id])
+    end
+    
+    def update
+        @financial = Financial.find(params[:id])
+        if @financial.update(
+            insititution: params[:financial][:insititution],
+            usable: params[:financial][:usable])
+            redirect_to financials_show_path
+        else
+            render edit_financial_path
+        end
+    end
+    
 end
